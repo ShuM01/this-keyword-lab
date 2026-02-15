d663d08deb73154733e596eeb28fb6a9d755ca04
shum01 <2021154167@ub.edu.bz>
2026-02-14 22:43:16 -0600

    complete part4

--- exercise4.js
+++ exercise4.js
@@ -2,12 +2,12 @@
 const box = document.querySelector('.box');
 box.addEventListener('click', (event) => {
  // BUG: this.classList.add('active') throws an error
-// this.classList.add('active');//
+// this.classList.add('active');
 event.currentTarget.classList.add('active');
 });
 
-//arrow function don't have their own "this".
+//Arrow function don't have their own "this".
 //they inherit "this" from the surrounding scope
 
 //so I use event.currentTarget instead of "this" to refer to the clicked element.
-//the listner is attached to the box element, This makes it equivalent to "this" in a regular function.
+//The listner is attached to the box element, This makes it equivalent to "this" in a regular function.
